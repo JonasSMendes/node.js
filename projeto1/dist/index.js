c5b67de05d76167ef9bf287e51c3784d3ef83dc9
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-03 09:49:36 -0300

    att
    projeto adicionando biblioteca e o typescript

---
+++ projeto1/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = 'jonas';
+let idade = 21;
+console.log(`meu nome ém ${nome} e minha idade é ${idade}`);
---
+++ projeto1/src/index.ts
@@ -0,0 +1,5 @@
+let nome:string = 'jonas'
+
+let idade:number = 21
+
+console.log(`meu nome ém ${nome} e minha idade é ${idade}`)
