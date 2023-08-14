0449fa619ad6568bad72444e3b282c7e60c74265
jonas mendes <jonas.jsm2903@gmail.com>
2023-08-14 17:00:16 -0300

    usando todos os gets,post,put e delete na api

--- node-mod-api/src/controllers/phrasesApi.ts
+++ node-mod-api/src/controllers/phrasesApi.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-
+import { Sequelize } from 'sequelize'
 import { Phrase } from "../models/Phrase";
 
 export const createPhrase = async (req: Request, res: Response) =>{
@@ -11,3 +11,68 @@ export const createPhrase = async (req: Request, res: Response) =>{
     res.status(201);
     res.json({id: newPhrase.id, author, txt});
 }
+
+export const listPhrases = async (req: Request, res: Response) =>{
+    let list = await Phrase.findAll()
+    res.json({list})
+}
+
+export const getPhrase = async (req: Request, res: Response) =>{
+    let {id} = req.params;
+
+    let phrase = await Phrase.findByPk(id);
+    if(phrase){
+        res.json({phrase})
+    }else{
+        res.json({error: 'deu algo de errado'})
+    }
+};
+
+
+export const updatePhrase = async (req: Request, res: Response) =>{
+    let {id} = req.params;
+    let {author, txt} = req.body;
+
+    let phrase = await Phrase.findByPk(id);
+    if(phrase){
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+
+        res.json({phrase});
+    }else{
+        res.json({error:'Frase não encontrada'})
+    }
+}
+
+export const deletePhrase = async (req: Request, res: Response) =>{
+    let {id} = req.params
+
+    await Phrase.destroy({
+        where:{
+            id: id
+        }
+    })
+
+    res.json({})
+}
+
+export const randomPhrase = async (req: Request, res: Response) => {
+    
+    try {
+        let phrase = await Phrase.findOne({
+            order: [
+                Sequelize.fn('RANDOM')
+            ]
+        });
+
+        if (phrase) {
+            res.json({ phrase });
+        } else {
+            res.status(404).json({ error: 'Não há frases encontradas' });
+        }
+    } catch (error) {
+        console.error('Erro ao buscar frase aleatória:', error);
+        res.status(500).json({ error: 'Ocorreu um erro ao buscar a frase aleatória' });
+    }
+};
--- node-mod-api/src/routes/api.ts
+++ node-mod-api/src/routes/api.ts
@@ -1,7 +1,7 @@
 import { match } from "assert";
 import { Router } from "express";
 import { ping, random, name } from "../controllers/apicontroller";
-import {createPhrase} from '../controllers/phrasesApi'
+import {createPhrase,listPhrases, getPhrase, updatePhrase, deletePhrase, randomPhrase } from '../controllers/phrasesApi'
 
 const router = Router();
 
@@ -9,7 +9,15 @@ router.get('/ping', ping);
 router.get('/random', random);
 router.get('/nome/:nome', name);
 
+
+router.get('/frase/aleatoria', randomPhrase)
 router.post('/frases', createPhrase )
+router.get('/frases', listPhrases)
+router.get('/frase/:id', getPhrase)
+router.put('/frase/:id', updatePhrase)
+router.delete('/frase/:id', deletePhrase)
+
+
 
 
 export default router
